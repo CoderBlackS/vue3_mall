79e59020c98989742945b12666ae4dcab89a8a2c
CoderBlackS <smallblacks@126.com>
2022-06-30 18:20:22 +0800

    'axios封装'

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
   <div id="app">
-    <router-link to="/login">登录</router-link>
-    <router-link to="/main">主页</router-link>
     <router-view></router-view>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-  name: 'App',
-  components: {}
-})
-</script>
+<script setup></script>
 
 <style lang="less"></style>
